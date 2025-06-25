6fdfa88bba19db7fdcdab12643e69167a2468515
Bu Kinoshita <bukinoshita@gmail.com>
2025-06-25 21:30:11 +0000

    feat: biome

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,9 @@
-import { render } from '@react-email/components'
-import { VercelInviteUserEmail } from 'transactional/emails/vercel-invite-user';
+import { render } from "@react-email/components";
+import { VercelInviteUserEmail } from "transactional/emails/vercel-invite-user";
 
 export default function Page(): JSX.Element {
-  const html = render(VercelInviteUserEmail({}));
+	const html = render(VercelInviteUserEmail({}));
 
-  return (
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-  );
+	// biome-ignore lint/security/noDangerouslySetInnerHtml: we need to render the email
+	return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
