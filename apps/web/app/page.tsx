fc152420651eb8298be529a098041feee9bf5adf
Gabriel Miranda <gabrielmfern@outlook.com>
2025-12-11 11:07:05 -0300

    chore: renovate (#77)
    * renovate workspace dependencies
    
    * update React Email dependencies
    
    * update turbo settings
    
    * ignore next-env.d.ts
    
    * update types
    
    * update next app
    
    * use an api route instead of a page
    
    * lint

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@react-email/components";
-import { VercelInviteUserEmail } from "transactional/emails/vercel-invite-user";
-
-export default function Page(): JSX.Element {
-	const html = render(VercelInviteUserEmail({}));
-
-	// biome-ignore lint/security/noDangerouslySetInnerHtml: we need to render the email
-	return <div dangerouslySetInnerHTML={{ __html: html }} />;
-}
